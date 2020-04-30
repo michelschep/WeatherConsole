70dfc91abf8afd1633d08cfa71ed3b7b02a4a692
Michel <michelschep@gmail.com>
2020-04-30 13:46:19 +0200

    Add project files.

---
+++ WeatherConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Hello World Weather Console");
+        }
+    }
+}
